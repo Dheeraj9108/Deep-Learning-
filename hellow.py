444da4ea0521c182e8a7b2cb5e62c15c9d2fe449
Dheeraj <poojarydheeraj665@gmail.com>
2022-10-10 21:57:26 +0530

    matrix

---
+++ hellow.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+rev = np.array([[180,200,220],[24,36,40],[12,18,20]])
+exp = np.array([[80,90,100],[10,16,20],[8,10,10]])
+
+profit = rev -exp
+print(profit)
