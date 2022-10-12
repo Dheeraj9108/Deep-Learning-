c044f68d5139c0d96ae34c39e69da3dafb8fdd47
Dheeraj <poojarydheeraj665@gmail.com>
2022-10-12 06:15:19 +0530

    SDG BGD MBGD

--- hellow.py
+++ hellow.py
@@ -1,8 +1,16 @@
-import numpy as np
-import matplotlib.pyplot as plt
 
-rev = np.array([[180,200,220],[24,36,40],[12,18,20]])
-exp = np.array([[80,90,100],[10,16,20],[8,10,10]])
+import pyautogui as p
+import time
 
-profit = rev -exp
-print(profit)
+time.sleep(4)
+for i in range (20):
+    p.typewrite("id card yav broo")
+    p.press('enter')
+
+
+# rev = np.array([[180,200,220],[24,36,40],[12,18,20]])
+# exp = np.array([[80,90,100],[10,16,20],[8,10,10]])
+
+# profit = rev -exp
+# print(profit)
+
